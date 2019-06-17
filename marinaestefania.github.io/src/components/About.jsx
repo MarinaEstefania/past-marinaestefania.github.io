7bedb27a92c677bcb31618773f9286b6adb4cad8
MarinaEstefania <m.garciac11@gmail.com>
2019-06-16 22:41:22 -0500

    pink navbar style

--- marinaestefania.github.io/src/components/About.jsx
+++ marinaestefania.github.io/src/components/About.jsx
@@ -13,10 +13,10 @@ class About extends Component {
                     <div class="col-sm-12 col-md-8 my-col">
                         <p >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
                     </div>
-                    <div class="col-sm-12 col-md-4 my-col my-flex">
+                    <div class="col-sm-12 col-md-4 my-col my-flex ">
                         {/* <img src={aboutImage}/> */}
-                        <div>
-                        <ul>
+                        <div  >
+                        <ul className="my-skills">
                             <li> React</li>
                             <li>Javascript</li>
                             <li>HTML 5</li>
@@ -28,7 +28,7 @@ class About extends Component {
                         </ul>
                         </div>
                          <div>
-                        <ul>
+                        <ul className="my-skills">
                             <li> React</li>
                             <li>Javascript</li>
                             <li>HTML 5</li>
